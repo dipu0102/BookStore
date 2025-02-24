97a9f3474b8d78e001b45d49ef070dd514743deb
dipankar <dipankargarai420@gmail.com>
2025-02-24 16:06:34 +0530

    Category Deletion Added

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -82,5 +82,35 @@ namespace BookStore.Controllers
 			return View();
 		}
 
+		public IActionResult Delete(int? id)
+		{
+
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		[ActionName("Delete")]
+
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _db.Categories.Find(id);
+			if (ModelState.IsValid)
+			{
+
+				_db.Categories.Remove(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
 	}
 }
