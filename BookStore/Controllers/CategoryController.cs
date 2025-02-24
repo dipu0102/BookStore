24671282d69cca5aa1a58498e3dce86f11a0e1d3
dipankar <dipankargarai420@gmail.com>
2025-02-24 15:00:31 +0530

    Add validation to Create Category

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -4,17 +4,44 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
 {
-    public class CategoryController : Controller
-    {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-        public IActionResult Index()
-        {
-            List<Category> CategoryList = _db.Categories.ToList();
-            return View(CategoryList);
-        }
-    }
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			List<Category> CategoryList = _db.Categories.ToList();
+			return View(CategoryList);
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+
+			if (obj.Name==obj.DisplayOrder.ToString()) {
+
+				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+			}
+			if (  obj.Name!=null && obj.Name.ToLower() == "test")
+			{
+
+				ModelState.AddModelError("", "Test is an invalid value");
+			}
+			if (ModelState.IsValid)
+			{
+
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+			return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+	}
 }
--- BookStore/Data/ApplicationDbContext.cs
+++ BookStore/Data/ApplicationDbContext.cs
@@ -3,22 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Data
 {
-    public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
 
-        }
-        public DbSet<Category> Categories { get; set; }
+		}
+		public DbSet<Category> Categories { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Category>().HasData(
-                new Category { Id=1,Name="Action",DisplayOrder="1"},
-                new Category { Id=2,Name="SciFi",DisplayOrder="2"},
-                new Category { Id=3,Name="History",DisplayOrder="3"}
-                );
-        }
-    }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Category>().HasData(
+				new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+				new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+				new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+				);
+		}
+	}
 }
