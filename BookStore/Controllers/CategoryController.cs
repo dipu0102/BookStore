2510d3f64c58abad7cc9ceb81a29dd316bcb5695
dipankar <dipankargarai420@gmail.com>
2025-02-24 15:41:14 +0530

    Added category Updation

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -24,11 +24,12 @@ namespace BookStore.Controllers
 		public IActionResult Create(Category obj)
 		{
 
-			if (obj.Name==obj.DisplayOrder.ToString()) {
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
 
 				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
 			}
-			if (  obj.Name!=null && obj.Name.ToLower() == "test")
+			if (obj.Name != null && obj.Name.ToLower() == "test")
 			{
 
 				ModelState.AddModelError("", "Test is an invalid value");
@@ -38,7 +39,45 @@ namespace BookStore.Controllers
 
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
-			return RedirectToAction("Index");
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public IActionResult Edit(int? id)
+		{
+
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+
+			//if (obj.Name == obj.DisplayOrder.ToString())
+			//{
+
+			//	ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+			//}
+			//if (obj.Name != null && obj.Name.ToLower() == "test")
+			//{
+
+			//	ModelState.AddModelError("", "Test is an invalid value");
+			//}
+			if (ModelState.IsValid)
+			{
+
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
 			}
 			return View();
 		}
